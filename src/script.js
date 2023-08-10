3a8fc695acd38ce46f24010414a08653f15fe3aa
ksrivera <ksrivera7@gmail.com>
2023-08-11 00:40:16 +0800

    fully responsive + menu function added

--- src/script.js
+++ src/script.js
@@ -5,52 +5,42 @@ const prevReviewer = document.getElementById("previous-reviewer")
 const cardWidth = slider.querySelector(".card").offsetWidth;
 
 //DOM MOBILE MENU
-const menuContainer = document.getElementById('mobile-container')
-const mobileMenu = document.getElementById("mobile-menu")
-const closeBtn = document.getElementById("close-btn")
-
-//MOBILE MENU TOGGLE -> to be added
-/* const closeMenu = () => {
-    menuContainer.innerHTML = ''
-}
+const menuContainer = document.getElementById('mobile-container') //not sure for this one yet
+const hamburger = document.getElementById("hamburger") //hamburger icon
 
 const openMenu = () => {
-   
-    menuContainer.innerHTML = ` 
-    <nav class="lg:hidden z-10 px-9 pt-8 h-full overflow-y-auto rounded-xl">
-        <div class="flex flex-wrap justify-between h-full">
-        <div class="w-full>
-            <div class="flex justify-between">
-                <div class="w-auto p-2"><a class="inline-block h-16 w-26" href="#"><img src="/public/assets/logo-no-background.png" class="h-16 w-26" alt="sample logo"/></a></div>
-                <div class="w-auto p-2"><i id="close-btn" class="fa-solid fa-square-xmark fa-2xl" style="color: var(--blue-color);"></i></div>
-            </div>
-        </div>
-        <div class="flex flex-col justify-center py-8 w-full">
-            <ul>
-            <li class="mb-9"><a class="inline-block text-lg font-bold text-stone-100 hover:text-gray-300 bg-blue-400 w-full rounded-lg p-3" href="#">Features</a></li>
-            <li class="mb-9"><a class="inline-block text-lg font-bold text-slate-100 hover:text-gray-300 bg-blue-400 w-full rounded-lg p-3" href="#">Solutions</a></li>
-            <li class="mb-9"><a class="inline-block text-lg font-bold text-slate-100 hover:text-gray-300 bg-blue-400 w-full rounded-lg p-3" href="#">Resources</a></li>
-            <li><a class="inline-block text-lg font-bold text-slate-100 hover:text-gray-300 bg-blue-400 w-full rounded-lg p-3" href="#">Pricing</a></li>
-            </ul>
-        </div>
-        <div class="flex flex-col justify-end w-full pb-8">
-            <div class="flex flex-wrap -m-2">
-            <div class="w-full p-2"><a class="block w-full px-4 py-2.5 text-sm text-center font-bold  text-black bg-transparent hover:bg-slate-300 ring-1 ring-slate-300 focus:ring-gray-600 rounded-full transition-all" href="#">Log In</a></div>
-            <div class="w-full p-2"><a class="block w-full px-4 py-2.5 text-sm text-center text-white font-bold bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-200 rounded-full" href="#">Get Started</a></div>
-            </div>
-        </div>
-        </div>
-    </nav>
-    `
-    if(menuContainer.classList.contains("close-btn")) {
-        console.log('true dat')
+    if (hamburger.classList.contains("fa-bars")) {
+        hamburger.classList.remove("fa-bars")
+        hamburger.classList.add("fa-xmark")
+        hamburger.classList.add("Capitalize")
+        hamburger.innerHTML = `
+            <nav class="lg:hidden absolute top-20 md:right-16 md:top-24 right-10 backdrop-blur-sm p-8 bg-slate-300/30 rounded-xl">
+                <div class="flex flex-wrap justify-between h-full">
+                    <div class="flex flex-col justify-center py-8 w-full">
+                        <ul >
+                            <li class="mb-9"><a class="inline-block text-center text-xl font-bold text-stone-100 hover:text-black bg-[var(--blue-color)] w-full rounded-lg p-2" href="#features"><span class="font-sans">Features<span></span></a></li>
+                            <li class="mb-9"><a class="inline-block text-center text-xl font-bold text-slate-100 hover:text-gray-300 bg-[var(--blue-color)] w-full rounded-lg p-2" href="#solutions"><span class="font-sans">Solutions</span></a></li>
+                            <li class="mb-9"><a class="inline-block text-center text-xl font-bold text-slate-100 hover:text-gray-300 bg-[var(--blue-color)] w-full rounded-lg p-2" href="#resources"><span class="font-sans">Resources</span></a></li>
+                            <li><a class="inline-block text-center text-xl font-bold text-slate-100 hover:text-gray-300 bg-[var(--blue-color)] w-full rounded-lg p-2" href="#pricing"><span class="font-sans">Pricing</span></a></li>
+                        </ul>
+                    </div>
+                    <div class="flex flex-col justify-end w-full pb-8">
+                        <div class="flex flex-wrap -m-2">
+                            <div class="w-full p-2"><a class="block w-full px-4 py-2.5 text-lg text-center font-bold  text-black bg-transparent hover:bg-slate-300 ring-1 ring-slate-300 focus:ring-gray-600 rounded-full transition-all" href="#login"><span class="font-sans">Log In</span></a></div>
+                            <div class="w-full p-2"><a class="block w-full px-4 py-2.5 text-lg text-center text-white font-bold bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-200 rounded-full" href="#get-started"><span class="font-sans">Get Started</a></span></div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        `
     }
 
+    else {
+        hamburger.classList.add("fa-bars")
+        hamburger.classList.remove("fa-xmark")
+        hamburger.innerHTML = '';
+    }
 }
- 
-mobileMenu.addEventListener("click", openMenu)
-
- */
 
 let isDragging = false, startX, startScrollLeft;
 
@@ -99,3 +89,4 @@ const dragging = (e) => {
 slider.addEventListener("mousedown", dragStart)
 slider.addEventListener("mousemove", dragging)
 document.addEventListener("mouseup", dragStop)
+menuContainer.addEventListener("click", openMenu)
