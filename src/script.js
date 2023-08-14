daacee08b6e5c77b03561eb323a174d4c775c172
ksrivera <ksrivera7@gmail.com>
2023-08-14 22:47:46 +0800

    v3.5 update

--- src/script.js
+++ src/script.js
@@ -1,15 +1,30 @@
-//DOM - SLIDER CONTENT
-const slider = document.getElementById("slider")
-const nextReviewer = document.getElementById("next-reviewer")
-const prevReviewer = document.getElementById("previous-reviewer")
-const cardWidth = slider.querySelector(".card").offsetWidth;
-
 //DOM - MOBILE MENU
 const menuContainer = document.getElementById('hamburger-menu') 
 const menuToggle = document.getElementById("menu-toggle")
 const toggleNav = document.getElementById("nav")
 const displayMenu = document.getElementById("display-menu")
 
+//DOM SwiperJS
+const mySwiper = document.getElementById("mySwiper")
+
+//SwiperJS
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    loop: true,
+    autoplay: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 //Mobile menu event listener
 menuContainer.addEventListener("click",  () => {
@@ -23,8 +38,9 @@ menuContainer.addEventListener("click",  () => {
 
         displayMenu.classList.add("absolute")
 
+
         displayMenu.innerHTML = `                            
-                <ul class="flex flex-col gap-10 p-5 h-screen backdrop-blur-lg bg-stone-200/30 rounded-b-2xl">
+                <ul class="flex flex-col gap-10 p-5 min-h-screen backdrop-blur-lg bg-stone-200/30 rounded-b-2xl">
                     <li class="list-none"><a class=" text-lg font-semibold text-black p-2" href="#features">Features</a></li>
                     <li class="list-none"><a class=" text-lg font-semibold text-black p-2" href="#solutions">Solutions</a></li>
                     <li class="list-none"><a class=" text-lg font-semibold text-black p-2" href="#resources">Resources</a></li>
@@ -46,48 +62,3 @@ menuContainer.addEventListener("click",  () => {
     }
 })
 
-let isDragging = false, startX, startScrollLeft;
-
-//REVIEWER ANIMATIONS
-//Next reviewer on mouse click or tap
-nextReviewer.addEventListener("click", () => {
-    slider.scrollLeft += -cardWidth;
-    slider.classList.add("scroll-smooth")
-    slider.classList.add("snap-x")
-})
-
-//previous reviewer on mouse click or tap
-prevReviewer.addEventListener("click", () => {
-    slider.scrollLeft += cardWidth;
-    slider.classList.add("scroll-smooth")
-    slider.classList.add("snap-x")
-})
-
-const dragStart = (e) => {
-    isDragging = true;
-    slider.classList.add("select-none")
-
-    //Records the initial cursor and scroll position of the slider
-    startX = e.pageX;
-    startScrollLeft = slider.scrollLeft;
-}
-
-const dragStop = () => {
-    isDragging = false;
-    slider.classList.remove("select-none")
-}
-
-const dragging = (e) => {
-    if(!isDragging) return
-    
-    slider.classList.remove("scroll-smooth")
-    slider.classList.remove("snap-x")
-
-    //Updates the scroll position of the slider based on the cursor movement
-    slider.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-
-//EVENT LISTENERS
-slider.addEventListener("mousedown", dragStart)
-slider.addEventListener("mousemove", dragging)
-document.addEventListener("mouseup", dragStop)
