a9c3ed968b41090cd7e394da7fe66d49c6063ff0
ksrivera <ksrivera7@gmail.com>
2023-08-14 10:58:45 +0800

    ready for merge

--- src/script.js
+++ src/script.js
@@ -5,43 +5,44 @@ const prevReviewer = document.getElementById("previous-reviewer")
 const cardWidth = slider.querySelector(".card").offsetWidth;
 
 //DOM - MOBILE MENU
-const menuContainer = document.getElementById('mobile-container') //not sure for this one yet
-const hamburger = document.getElementById("hamburger") //hamburger icon
+const menuContainer = document.getElementById('hamburger-menu') 
+const menuToggle = document.getElementById("menu-toggle")
+const toggleNav = document.getElementById("nav")
+const displayMenu = document.getElementById("display-menu")
 
-//open-close hamburger menu function
-const toggleMenu = () => {
-    if (hamburger.classList.contains("fa-bars")) {
-        hamburger.classList.remove("fa-bars")
-        hamburger.classList.add("fa-xmark")
-        
-        hamburger.innerHTML = `
-            <nav class="lg:hidden absolute top-20 md:right-16 md:top-24 right-10 backdrop-blur-sm p-8 bg-slate-300/30 rounded-xl">
-                <div class="flex flex-wrap justify-between h-full">
-                    <div class="flex flex-col justify-center py-8 w-full">
-                        <ul >
-                            <li class="mb-9"><a class="inline-block text-center text-xl font-bold text-stone-100 hover:text-black bg-[var(--blue-color)] w-full rounded-lg p-2" href="#features"><span class="font-sans">Features<span></span></a></li>
-                            <li class="mb-9"><a class="inline-block text-center text-xl font-bold text-slate-100 hover:text-gray-300 bg-[var(--blue-color)] w-full rounded-lg p-2" href="#solutions"><span class="font-sans">Solutions</span></a></li>
-                            <li class="mb-9"><a class="inline-block text-center text-xl font-bold text-slate-100 hover:text-gray-300 bg-[var(--blue-color)] w-full rounded-lg p-2" href="#resources"><span class="font-sans">Resources</span></a></li>
-                            <li><a class="inline-block text-center text-xl font-bold text-slate-100 hover:text-gray-300 bg-[var(--blue-color)] w-full rounded-lg p-2" href="#pricing"><span class="font-sans">Pricing</span></a></li>
-                        </ul>
-                    </div>
-                    <div class="flex flex-col justify-end w-full pb-8">
-                        <div class="flex flex-wrap -m-2">
-                            <div class="w-full p-2"><a class="block w-full px-4 py-2.5 text-lg text-center font-bold  text-black bg-transparent hover:bg-slate-300 ring-1 ring-slate-300 focus:ring-gray-600 rounded-full transition-all" href="#login"><span class="font-sans">Log In</span></a></div>
-                            <div class="w-full p-2"><a class="block w-full px-4 py-2.5 text-lg text-center text-white font-bold bg-[var(--blue-color)] hover:bg-blue-600 focus:ring-4 focus:ring-blue-200 rounded-full" href="#get-started"><span class="font-sans">Get Started</a></span></div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        `
+
+//Mobile menu event listener
+menuContainer.addEventListener("click",  () => {
+    menuToggle.classList.toggle("fa-xmark")
+
+    if(menuToggle.classList.contains('fa-xmark')) {
+        toggleNav.classList.remove("bg-white")
+        toggleNav.classList.remove("rounded-full")
+        toggleNav.classList.add("bg-stone-200/30")
+        toggleNav.classList.add("rounded-t-2xl")
+        displayMenu.innerHTML = `                            
+            <div class="flex flex-col justify-center p-4 gap-3 bg-stone-200/30 rounded-b-2xl backdrop-blur-lg transition-all">
+                <ul class="flex flex-col gap-3">
+                    <li><a class="flex text-lg font-semibold text-black  p-2" href="#features">Features</a></li>
+                    <li><a class="flex text-lg font-semibold text-black p-2" href="#solutions">Solutions</a></li>
+                    <li><a class="flex text-lg font-semibold text-black  p-2" href="#resources">Resources</a></li>
+                    <li><a class="flex text-lg font-semibold text-black  p-2" href="#pricing">Pricing</a></li>
+                    <li><a class="flex text-lg font-semibold text-red-500  p-2" href="#pricing">Log In</a></li>
+                    <li><a class="flex text-lg font-semibold text-[var(--blue-color)] p-2" href="#resources">Get Started</a></li>
+                </ul>
+            </div>
+
+        ` 
     }
 
     else {
-        hamburger.classList.add("fa-bars")
-        hamburger.classList.remove("fa-xmark")
-        hamburger.innerHTML = '';
+        displayMenu.innerHTML = '';
+        toggleNav.classList.remove("rounded-t-2xl")
+        toggleNav.classList.remove("bg-stone-200/30")
+        toggleNav.classList.add("rounded-full")
+        toggleNav.classList.add("bg-white")
     }
-}
+})
 
 let isDragging = false, startX, startScrollLeft;
 
@@ -88,4 +89,3 @@ const dragging = (e) => {
 slider.addEventListener("mousedown", dragStart)
 slider.addEventListener("mousemove", dragging)
 document.addEventListener("mouseup", dragStop)
-menuContainer.addEventListener("click", toggleMenu) //mobile menu
